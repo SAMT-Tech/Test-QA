e53b6bff7746f4f91e21b05fdcfda17ebbe21455
ArchanaRR <36619202+ArchanaRR@users.noreply.github.com>
2018-02-19 16:06:41 +0530

    Update spec.js

--- spec.js
+++ spec.js
@@ -1,8 +1,8 @@
 describe('NBL Login Validation', function() {
  it('Login With Valid Data', function() {
  browser.get('https://msmecor.com/app/account#/login');
- element(by.model('username')).sendKeys('nbl'); 
- element(by.model('password')).sendKeys('msm12!!2017');
+ element(by.id('username')).sendKeys('nbl'); 
+ element(by.id('password')).sendKeys('msm12!!2017');
  element(by.xpath('input[@value='Login']')).click();
  var dashboardTitle= element(by.xpath('div[@class='dashboard-title']').getText();
  expect(dashboardTitle).toEqual('NBL Main Dashboard (Beta)');
